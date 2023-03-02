6073dd8d34ababac90eb6f549d467db88ff9b609
@JayBatu <ba2demirtas@gmail.com>
2023-03-02 14:59:54 -0800

    First set of code using the GIPHY API.

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 import requests
 import json
+import os
+
+client = discord.Client()
 
 def run_discord_bot():
     TOKEN = 'MTA4MDQxNjY2NTc0Mjg3NjgwMw.G18JAE.ewOtI_aVfMxJS3SenLaeK-0JYqdujoFL8BJ75U'
@@ -18,9 +21,11 @@ def run_discord_bot():
     async def on_message(message):
         if message.author == client.user:
             return
+        if message.content.startswith('gif ') and message.content.endswith('.'):
+            return None
 
-        if message.content.lower().startswith("ege"):
-            await message.channel.send("ege harbi gay mq")
+        elif message.content.lower().startswith("ege"):
+            await message.channel.send("ege = GAY TROLOLOLOLOOOO!!!!!")
 
         elif message.content[0] == '?' or message.content[0]== '!' or message.content[0] == '.' or message.content[0] == '$' or message.content[0] == '@' or message.content[0] == '#':
             message.content = message.content[1:]
@@ -39,20 +44,41 @@ def run_discord_bot():
         elif 'talha' in message.content.lower():
             await message.channel.send("I'm nobody. I'm nowhere. I drive. I. Am. Him.")
 
-    client.run(TOKEN)
 
-    giphy_api_key = "fXAagJPL2EA8yvLp2Dpx9SbjiSP0vskl"
+
+    api_key = "fXAagJPL2EA8yvLp2Dpx9SbjiSP0vskl"
     giphy_endpoint = "http://api.giphy.com/v1/gifs/search"
 
-    def gif_response(message):
+    def get_gif_url(query):
+
+        api_key = "fXAagJPL2EA8yvLp2Dpx9SbjiSP0vskl"
+
+        url = f"http://api.giphy.com/v1/gifs/search?q={query}&api_key={api_key}&limit=1"
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            gif_url = data["data"][0]["images"]["original"]["url"]
+            return gif_url
+        else:
+            return None
+
+    @client.event
+    async def on_ready():
+        print(f"Logged in as {client.user}")
+
+    @client.event
+    async def gif_response(message):
         if message.content.startswith('gif ') and message.content.endswith('.'):
             search_term = message.content[4:-1]
+            gif_url = get_gif_url(search_term)
+            if gif_url:
+                await message.channel.send(gif_url)
+            else:
+                await message.channel.send('Oyle bir gif yok aptal maymun')
 
-
-
-
-
-
+    client.run(TOKEN)
 
 
 
---
+++ gifresponse.py
@@ -0,0 +1,5 @@
+import discord
+import json
+import os
+import requests
+from dotenv import dotenv
